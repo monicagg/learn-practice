ddc468d70d461978730a02848353e2b2470b3d06
monicagg <monica.guzik@gmail.com>
2020-06-03 15:10:31 +0100

    Project integrates Angular framework with D3js library

---
+++ angular-d3/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BarChartComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
